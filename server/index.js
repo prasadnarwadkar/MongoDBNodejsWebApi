f727248fea34e0acae3b527b1dc90319f6e33a5a
Prasad Narwadkar <prasad.narwadkar@live.in>
2025-03-22 22:09:37 +0530

    CORS related changes

--- server/index.js
+++ server/index.js
@@ -4,8 +4,23 @@ const app = require('./config/express');
 require('./config/mongoose');
 const cors = require('cors')
 
-app.use(cors({ origin: '*', allowedHeaders:'*',methods:'*'}));
-app.options('*', cors());
+//app.use(cors({ origin: '*', allowedHeaders:'*',methods:'*'}));
+//app.options('*', cors());
+
+// Custom CORS middleware
+const corsMiddleware = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).json({});
+  }
+  next();
+};
+
+// Use the custom middleware
+app.use(corsMiddleware);
 
 // Hello World for index page
 app.get('/', function (req, res) {
